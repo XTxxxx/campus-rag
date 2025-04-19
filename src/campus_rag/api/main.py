2debc3f78c962d41fd2f4731a1d8e3d7615ac756
xtx <xtianx@smail.nju.edu.cn>
2025-04-19 21:55:24 +0800

    merge data

--- src/campus_rag/api/main.py
+++ src/campus_rag/api/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from src.campus_rag.api.routes import rag, conversation
+from src.campus_rag.utils.logging_config import configure_logger
 
+logger = configure_logger()
+
+logger.info("Starting FastAPI application...")
 app = FastAPI()
 
 app.include_router(rag.router)
